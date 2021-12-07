e9b98852733049730d23aee7942a5d4ae8b438be
13010-55-06 <13010-55-06@exchange.ad.afpanet>
2021-12-07 13:24:27 +0100

    responsive compétence

--- competence/competence.js
+++ competence/competence.js
@@ -2,9 +2,15 @@ var btn = document.querySelectorAll('.choix_competence');
 var ensemble = document.querySelectorAll('.ensemble');
  
 for(var i =0; i<btn.length; i++) {
+    
     btn[i].addEventListener('click', (e)=> {
-    var detail = e.path[1].children[1].children[0];
-    console.log(detail)
-    detail.style.transform = 'translateX(0)';
+    //  console.log(); 
+        var detail = e.target.nextElementSibling.children[0];
+        detail.style.transform = 'translateX(0)';
+        detail.style.webkitTransform = 'translateX(0)';
+        detail.style.mozTransform = 'translateX(0)';
+        detail.style.msTransform = 'translateX(0)';
+        detail.style.oTransform = 'translateX(0)';
+   
     })
 }
